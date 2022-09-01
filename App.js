ba727f742b1cbc6476fd5088395a950656ca0eeb
Michael Paukov <michael.paukov@gmail.com>
2022-09-01 11:55:15 +0200

    TextInput

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+} from "react-native";
+
+const image = require("./assets/images/stars-on-night.jpg");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I can see it on my phone</Text>
+      <ImageBackground source={image} style={styles.image}>
+        <TextInput style={styles.input} textAlign={"center"} />
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +24,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+  },
+  image: {
+    flex: 1,
+    // alignItems: "center",
     justifyContent: "center",
+    resizeMode: "cover",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#f0f8ff",
+    color: "#f0f8ff",
+    marginHorizontal: 40,
+    height: 40,
+    borderRadius: 6,
   },
 });
